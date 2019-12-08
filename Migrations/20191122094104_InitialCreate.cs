0e949614087a237605cc538f7f4e973c33f2f045
Kiran <kiranvirk988@gmail.com>
2019-12-08 22:08:47 +1300

    Add project files.

---
+++ Migrations/20191122094104_InitialCreate.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tech_Meetups_Final_Project.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Meetup",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EventTitle = table.Column<string>(nullable: true),
+                    Venue = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    StartTime = table.Column<DateTime>(nullable: false),
+                    EndTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Meetup", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Participant",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Mobile = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Participant", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MeetupRegistration",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MeetupId = table.Column<int>(nullable: false),
+                    ParticipantId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MeetupRegistration", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MeetupRegistration_Meetup_MeetupId",
+                        column: x => x.MeetupId,
+                        principalTable: "Meetup",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MeetupRegistration_Participant_ParticipantId",
+                        column: x => x.ParticipantId,
+                        principalTable: "Participant",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MeetupRegistration_MeetupId",
+                table: "MeetupRegistration",
+                column: "MeetupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MeetupRegistration_ParticipantId",
+                table: "MeetupRegistration",
+                column: "ParticipantId");
+
+            var sqlFile = Path.Combine(".\\DatabaseScripts", @"data.sql");
+
+            migrationBuilder.Sql(File.ReadAllText(sqlFile));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MeetupRegistration");
+
+            migrationBuilder.DropTable(
+                name: "Meetup");
+
+            migrationBuilder.DropTable(
+                name: "Participant");
+        }
+    }
+}
